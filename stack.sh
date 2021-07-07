155f7ca1c8dfa5a0b238a2c3c82c0e14e2c27226
yonnyviz <yonnyviz@gmail.com>
2021-07-07 18:06:51 -0500

    Implementacion de shellscript con funcionalidades para automatizar integracion y despliegue

--- stack.sh
+++ stack.sh
@@ -4,17 +4,6 @@
 # Version: 1.0.0
 # Description: Wrapper Scripts Automation Tool.
 
-#
-# imports
-#
-#source $(pwd)"/modules/terraform-functions.sh"
-#source $(pwd)"/modules/gcp-functions.sh"
-
-#
-# Paths definitions
-#
-TERRAFORM_MODULE_PATH=$(pwd)"/terraform"
-
 function create() {
     if [[ ! -z ${1} ]]; then
         echo "INFO - Creating Tech Stack"
@@ -33,33 +22,29 @@ function destroy() {
     fi
 }
 
-function createSpecificStack() {
-    docker-compose -f "docker-compose.yml" up -d --build db
+function buildImage() {
+    local DOCKERFILE_PATH=${1}
+
+    docker build ${DOCKERFILE_PATH}
 }
 
-function argParser() {
-    shift
-    while getopts "f:d:n:" opt; do
-        case ${opt} in
-        f)
-            COMPOSE_FILE=${OPTARG}
-            ;;
-        d)
-            KEY_FILE_PATH=${OPTARG}
-            ;;
-        n)
-            APP_NAME=${OPTARG}
-            ;;
-        \?)
-            echo "Dev help test"
-            exit 1
-            ;;
-        esac
-    done
-    shift $((OPTIND - 1))
+function pushImage() {
+    local IMAGE_NAME=${1}
+    local IMAGE_TAG=${2}
+
+    docker push localhost:5000/${IMAGE_NAME}:${IMAGE_TAG}
 }
 
-argParser $@
+function pullImage() {
+    local IMAGE_NAME=${1}
+
+    docker pull ${IMAGE_NAME}
+}
+
+function tagImage() {
+    local IMAGE_NAME=${1}
+    docker tag ${IMAGE_NAME} ${IMAGE_NAME}:$(git rev-parse --short HEAD)
+}
 
 if [[ $# -gt 0 ]]; then
     if [[ $1 == "create" ]]; then
@@ -68,8 +53,17 @@ if [[ $# -gt 0 ]]; then
     if [[ $1 == "destroy" ]]; then
         destroy $2
     fi
-    if [[ $1 == "output" ]]; then
-        echo "Not Implemented"
+    if [[ $1 == "buildImage" ]]; then
+        buildImage $2
+    fi
+    if [[ $1 == "pushImage" ]]; then
+        pushImage $2
+    fi
+    if [[ $1 == "pullImage" ]]; then
+        pullImage $2
+    fi
+    if [[ $1 == "tagImage" ]]; then
+        tagImage $2
     fi
 else
     echo "
